781101fb1f67d736f18866f07a1088984acfb6b8
MauroSpagnoli <maurospagnoliherrera@hotmail.com>
2022-03-08 11:25:54 +0100

    Final commit

--- src/app/pais/components/pais-input/pais-input.component.ts
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, EventEmitter, Output, OnInit, Input } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pais-input',
@@ -7,27 +8,33 @@ import { debounceTime, Subject } from 'rxjs';
   styles: [
   ]
 })
-export class PaisInputComponent implements OnInit{
+export class PaisInputComponent implements OnInit {
   
-  @Output()onEnter:EventEmitter<string> = new EventEmitter();
-  @Output()onDebouce:EventEmitter<string> = new EventEmitter();
-  @Input()placeholder:string= '';
+
+  @Output() onEnter   : EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  @Input() placeholder: string = '';
+
   debouncer: Subject<string> = new Subject();
 
-  termino:string='';
+  termino: string = '';
 
-  ngOnInit(){
-    this.debouncer.pipe(debounceTime(300)).subscribe(valor =>{
-      this.onDebouce.emit(valor);
-    });
+  ngOnInit() {
+    this.debouncer
+      .pipe(debounceTime(300))
+      .subscribe( valor => {
+        this.onDebounce.emit( valor );
+      });
   }
 
-  buscar(){
-    this.onEnter.emit(this.termino);
+  buscar() {
+    this.onEnter.emit( this.termino );
   }
 
-  teclaPresionada(){
-    this.debouncer.next(this.termino);
+  teclaPresionada() {
+    this.debouncer.next( this.termino );
   }
+  
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
@@ -9,21 +9,33 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl: string = 'https://restcountries.com/v3.1'
+  private apiUrlRegion: string = 'https://restcountries.com/v2'
+
+  get httpParams(){
+    return new HttpParams()
+      .set('Fields','name,capital,alpha2Code,flag,population');
+  }
 
   constructor(private http:HttpClient) { }
 
   buscarPais(termino:string): Observable<Country[]>{
     const url =`${this.apiUrl}/name/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params:this.httpParams});
   }
 
   buscarCapital(termino:string): Observable<Country[]>{
     const url =`${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url,{params:this.httpParams});
   }
 
   getPaisPorCodigo(id:string): Observable<Country>{
     const url =`${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country>(url);
+  }
+
+  buscarRegion(region:string):Observable<Country[]>{
+    
+    const url =`${this.apiUrlRegion}/regionalbloc/${region}`;
+    return this.http.get<Country[]>(url,{params:this.httpParams}).pipe(tap(console.log));
   }
 }
